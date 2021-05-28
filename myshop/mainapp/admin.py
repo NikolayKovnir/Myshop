4f8af87242739aa9306cbae3adea3c2cac58932f
nikolay <nikolaykovnir91@gmail.ru>
2021-05-28 19:06:10 +0300

    inheritance in templates

--- myshop/mainapp/admin.py
+++ myshop/mainapp/admin.py
@@ -1,40 +1,10 @@
-from PIL import Image
-from django.forms import ModelChoiceField, ModelForm, ValidationError
+from django.forms import ModelChoiceField
 from django.contrib import admin
-from django.utils.safestring import mark_safe
 from django import forms
 from .models import *
 
 
-class NotebookAdminForm(ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = mark_safe(
-            """<span style="color:red; font-size:14px">при загрузке изображения с разрешением больше {}x{} оно будет обрезано</span>
-            """
-                .format(
-                *Product.MIN_RESOLUTION
-            )
-        )
-    #
-    # def Clean_image(self):
-    #     image = self.cleaned_data['image']
-    #     img = Image.open(image)
-    #     min_height, min_width = Product.MIN_RESOLUTION
-    #     max_height, max_width = Product.MAX_RESOLUTION
-    #     if image.size > Product.MAX_IMAGE_SIZE:
-    #         raise ValidationError('Размер изображения не должен превышать 3МБ')
-    #     if img.height < min_height or img.width < min_width:
-    #         raise ValidationError('разрешение изображения меньше минимального')
-    #     if img.height > max_height or img.width > max_width:
-    #         raise ValidationError('разрешение изображения больше минимального')
-    #     return image
-
-
 class NotebookAdmin(admin.ModelAdmin):
-
-    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
--- myshop/mainapp/models.py
+++ myshop/mainapp/models.py
@@ -1,22 +1,16 @@
-import sys
-from PIL import Image
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
-from io import BytesIO
-from django.core.files.uploadedfile import InMemoryUploadedFile
-
+from django.urls import reverse
 
 User = get_user_model()
 
 
-class MinResolutionErorExeption(Exception):
-    pass
+def get_product_url(obj, viewname):
+    ct_model = obj.__class__.meta.model_name
+    return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
 
-
-class MaxResolutionErorExeption(Exception):
-    pass
 
 
 class LatestProductsManager:
@@ -74,28 +68,6 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
-    def save(self, *args, **kwargs):
-        # image = self.image
-        # img = Image.open(image)
-        # min_height, min_width = self.MIN_RESOLUTION
-        # max_height, max_width = self.MAX_RESOLUTION
-        # if img.height < min_height or img.width < min_width:
-        #     raise MinResolutionErorExeption('разрешение изображения меньше минимального')
-        # if img.height > max_height or img.width > max_width:
-        #     raise MaxResolutionErorExeption
-        image = self.image
-        img = Image.open(image)
-        new_img = img.convert('RGB')
-        resized_new_img = new_img.resize((200, 200), Image.ANTIALIAS)
-        filestream = BytesIO()
-        resized_new_img.save(filestream, 'JPEG', quality=90)
-        filestream.seek(0)
-        name = '{}.{}'.format(*self.image.name.split('.'))
-        self.image = InMemoryUploadedFile(
-            filestream, 'ImageField', name, 'jpeg/image', sys.getsizeof(filestream), None
-        )
-        super().save(*args, **kwargs)
-
 
 class Notebook(Product):
 
@@ -108,6 +80,9 @@ class Notebook(Product):
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class Smartphone(Product):
@@ -124,6 +99,9 @@ class Smartphone(Product):
 
     def __str__(self):
         return "{} : {}".format(self.category.name, self.title)
+
+    def get_absolute_url(self):
+        return get_product_url(self, 'product_detail')
 
 
 class CartProduct(models.Model):
