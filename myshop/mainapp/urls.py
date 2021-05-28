ee077108b8a5e92e7f464a7b538c268328e579f2
nikolay <nikolaykovnir91@gmail.ru>
2021-05-28 19:57:25 +0300

    templates spec working version

--- myshop/mainapp/urls.py
+++ myshop/mainapp/urls.py
@@ -5,4 +5,5 @@ from .views import test_view, ProductDetailView
 urlpatterns = [
     path('', test_view, name='base'),
     path('products/<str:ct_model>/<str:slug>/', ProductDetailView.as_view(), name='product_detail'),
+
 ]
