e07144a6741717cbb3b7b64cf18d2ccd1d5fd959
nikolay <nikolaykovnir91@gmail.ru>
2021-06-03 23:53:43 +0300

    made model Order

--- myshop/mainapp/models.py
+++ myshop/mainapp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.urls import reverse
+from django.utils import timezone
 
 User = get_user_model()
 
@@ -33,18 +34,16 @@ class LatestProductsManager:
             if ct_model.exists():
                 if with_respect_to in args:
                     return sorted(
-                       products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to),
-                       reverse=True)
+                        products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to),
+                        reverse=True)
         return products
 
 
 class LatestProducts:
-
     objects = LatestProductsManager()
 
 
 class CategoryManager(models.Manager):
-
     CATEGORY_NAME_COUNT_NAME = {
         'Ноутбуки': 'notebook__count',
         'Смартфоны': 'smartphone__count'
@@ -64,7 +63,6 @@ class CategoryManager(models.Manager):
 
 
 class Category(models.Model):
-
     name = models.CharField(max_length=255, verbose_name="Имя категории")
     slug = models.SlugField(unique=True)
     objects = CategoryManager()
@@ -77,7 +75,6 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-
     class Meta:
         abstract = True
 
@@ -96,7 +93,6 @@ class Product(models.Model):
 
 
 class Notebook(Product):
-
     diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
     display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
     processor_freq = models.CharField(max_length=255, verbose_name='Частота процессора')
@@ -112,7 +108,6 @@ class Notebook(Product):
 
 
 class Smartphone(Product):
-
     diagonal = models.CharField(max_length=255, verbose_name='Диаганоль')
     display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
     resolution = models.CharField(max_length=250, verbose_name='Разрешение Экрана')
@@ -132,9 +127,7 @@ class Smartphone(Product):
         return get_product_url(self, 'product_detail')
 
 
-
 class CartProduct(models.Model):
-
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
@@ -152,8 +145,7 @@ class CartProduct(models.Model):
 
 
 class Cart(models.Model):
-
-    owner = models.ForeignKey('Customer', null=True,  verbose_name='Владелец', on_delete=models.CASCADE)
+    owner = models.ForeignKey('Customer', null=True, verbose_name='Владелец', on_delete=models.CASCADE)
     products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9, default=0, decimal_places=2, verbose_name='Общая цена')
@@ -174,12 +166,58 @@ class Cart(models.Model):
 
 
 class Customer(models.Model):
-
     user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name='номер телефона', null=True, blank=True)
     adres = models.CharField(max_length=255, verbose_name='адрес', null=True, blank=True)
+    orders = models.ManyToManyField('Order', verbose_name='Заказы покупателя', related_name='related_order')
 
     def __str__(self):
         return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
 
 
+class Order(models.Model):
+    STATUS_NEW = 'new'
+    STATUS_IN_PROGRESS = 'in_progress'
+    STATUS_READY = 'ready'
+    STATUS_COMPLETED = 'completed'
+
+    BUYING_TYPE_SELF = 'self'
+    BUYING_TYPE_DELIVERY = 'delivery'
+
+    STATUS_CHOICES = (
+        (STATUS_NEW, 'Новый заказ'),
+        (STATUS_IN_PROGRESS, 'Заказ в обработке'),
+        (STATUS_READY, 'Заказ готов'),
+        (STATUS_COMPLETED, 'Заказ выполнен')
+    )
+
+    BUYING_TYPE_CHOICES = (
+        (BUYING_TYPE_SELF, 'Самовывоз'),
+        (BUYING_TYPE_DELIVERY, 'Доставка')
+    )
+
+    customer = models.ForeignKey(
+        Customer, verbose_name='Покупатель', related_name='related_orders', on_delete=models.CASCADE
+    )
+    first_name = models.CharField(max_length=255, verbose_name='Имя')
+    last_name = models.CharField(max_length=255, verbose_name='Фамилия')
+    phone = models.CharField(max_length=20, verbose_name='Телефон')
+    adress = models.CharField(max_length=1024, verbose_name='Адресс', null=True, blank=True)
+    status = models.CharField(
+        max_length=100,
+        verbose_name='Статус заказ',
+        choices=STATUS_CHOICES,
+        default=STATUS_NEW
+    )
+    buying_type = models.CharField(
+        max_length=100,
+        verbose_name='Тип заказа',
+        choices=BUYING_TYPE_CHOICES,
+        default=BUYING_TYPE_SELF
+    )
+    comment = models.TextField(verbose_name='Комментарий к заказу', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания заказа')
+    order_date = models.DateField(verbose_name='Дата получения заказа', default=timezone.now)
+
+    def __str__(self):
+        return str(self.id)
