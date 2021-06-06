86d8a3268695787417c019af9a4b5283d82ba57b
nikolay <nikolaykovnir91@gmail.ru>
2021-06-06 16:29:10 +0300

    api pagination second var

--- myshop/mainapp/api/api_views.py
+++ myshop/mainapp/api/api_views.py
@@ -1,11 +1,30 @@
+from rest_framework.response import Response
+from collections import OrderedDict
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 from .serializers import CategorySerializers, SmartphoneSerializer, NotebookSerializer, CustomerSerializer
 from ..models import Category, Smartphone, Notebook, Customer
 
 
+class CategoryPagination(PageNumberPagination):
+
+    page_size = 2
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('objects_count', self.page.paginator.count),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('items', data)
+        ]))
+
+
 class CategoryListAPIView(ListAPIView):
     serializer_class = CategorySerializers
+    pagination_class = CategoryPagination
     queryset = Category.objects.all()
 
 
