a8de982c0c5b3a89d850883931fe24334f726f3a
nikolay <nikolaykovnir91@gmail.ru>
2021-06-08 01:08:04 +0300

    refactoring(delite abstract models notebook/smartphones)

--- myshop/mainapp/api/api_views.py
+++ myshop/mainapp/api/api_views.py
@@ -3,8 +3,8 @@ from collections import OrderedDict
 from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView, RetrieveUpdateAPIView
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
-from .serializers import CategorySerializers, SmartphoneSerializer, NotebookSerializer, CustomerSerializer
-from ..models import Category, Smartphone, Notebook, Customer
+from .serializers import CategorySerializers, CustomerSerializer
+from ..models import Category, Customer
 
 
 class CategoryPagination(PageNumberPagination):
@@ -26,26 +26,6 @@ class CategoryListAPIView(ListCreateAPIView, RetrieveUpdateAPIView):
     serializer_class = CategorySerializers
     pagination_class = CategoryPagination
     queryset = Category.objects.all()
-    lookup_field = 'id'
-
-
-class SmartphoneListAPIView(ListAPIView):
-    serializer_class = SmartphoneSerializer
-    queryset = Smartphone.objects.all()
-    filter_backends = [SearchFilter]
-    search_fields = ['price', 'title']
-
-
-class NotebookListAPIView(ListAPIView):
-    serializer_class = NotebookSerializer
-    queryset = Notebook.objects.all()
-    filter_backends = [SearchFilter]
-    search_fields = ['price', 'title']
-
-
-class SmartphoneDetailView(RetrieveAPIView):
-    serializer_class = SmartphoneSerializer
-    queryset = Smartphone.objects.all()
     lookup_field = 'id'
 
 
--- myshop/mainapp/api/urls.py
+++ myshop/mainapp/api/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 
 from .api_views import (
     CategoryListAPIView,
-    SmartphoneListAPIView,
-    NotebookListAPIView,
-    SmartphoneDetailView,
     CustomerListAPIview
 )
 
@@ -12,7 +9,4 @@ from .api_views import (
 urlpatterns = [
     path('categories/<str:id>', CategoryListAPIView.as_view(), name='categories'),
     path('customers/', CustomerListAPIview.as_view(), name='customers_list'),
-    path('smartphones/', SmartphoneListAPIView.as_view(), name='smartphones_list'),
-    path('notebooks/', NotebookListAPIView.as_view(), name='notebooks_list'),
-    path('smartphones/<str:id>', SmartphoneDetailView.as_view(), name='smartphone_detail'),
 ]
