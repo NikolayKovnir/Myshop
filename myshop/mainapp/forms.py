518389bf64ae27744c1b00ec4e044782c44351e5
nikolay <nikolaykovnir91@gmail.ru>
2021-06-09 19:46:03 +0300

    panel registration

--- myshop/mainapp/forms.py
+++ myshop/mainapp/forms.py
@@ -9,7 +9,7 @@ class OrderForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['order_date'].label = "Дата получения заказа"
 
-    order_date = forms.DateField(widget=forms.TextInput(attrs={'type':'date'}))
+    order_date = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
 
     class Meta:
         model = Order
@@ -19,7 +19,6 @@ class OrderForm(forms.ModelForm):
 
 
 class LoginForm(forms.ModelForm):
-
     password = forms.CharField(widget=forms.PasswordInput)
 
     def __init__(self, *args, **kwargs):
@@ -41,3 +40,51 @@ class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class RegistrationForm(forms.ModelForm):
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    phone = forms.CharField(required=False)
+    address = forms.CharField(required=False)
+    email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = "Логин"
+        self.fields['password'].label = "Пароль"
+        self.fields['confirm_password'].label = "Подтвердите пароль"
+        self.fields['phone'].label = "телефон"
+        self.fields['first_name'].label = "Ваше имя"
+        self.fields['last_name'].label = "Фамилия"
+        self.fields['address'].label = "адрес"
+        self.fields['email'].label = "почта"
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        domain = email.split('.')[-1]
+        if domain in ['com', 'net']:
+            raise forms.ValidationError(f'регистрация для домена "{domain}" невозможна')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Данный почтовый адрес уже зарегестрирован в системе')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'Имя {username} занято')
+        return username
+
+    def clean(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('введенные пароли не совпадают')
+        return self.cleaned_data
+
+    class Meta:
+        model = User
+        fields = [
+            'username', 'password', 'confirm_password', 'first_name', 'last_name', 'address', 'phone', 'email'
+        ]
